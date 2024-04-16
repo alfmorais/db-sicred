796776d2f33fd7fe359ae4ff83349ddee7ccf5f8
alfmorais <alfredneto@hotmail.com>
2024-04-16 20:59:24 -0300

    Feat: otimize first question to use generator

--- src/question1.py
+++ src/question1.py
@@ -1,12 +1,30 @@
+from typing import List
+
+
 class Contract:
-    def __init__(self, id, debt):
+    def __init__(self, id: int, debt: int) -> None:
         self.id = id
         self.debt = debt
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "id={}, debt={}".format(self.id, self.debt)
 
 
 class Contracts:
-    def get_top_N_open_contracts(self, open_contracts, renegotiated_contracts, top_n):
-        pass
+    def get_top_N_open_contracts(
+        self,
+        open_contracts: List[Contract],
+        renegotiated_contracts: List[int],
+        top_n: int,
+    ):
+        filter_contracts = (
+            contract
+            for contract in open_contracts
+            if contract.id not in renegotiated_contracts
+        )
+        sorted_contracts = sorted(
+            filter_contracts,
+            key=lambda contract: contract.debt,
+            reverse=True,
+        )
+        return [contract.id for contract in sorted_contracts[:top_n]]
